8fb7ace866bd2f0fadca800fe560bb59ea7c255a
The4codeblocks <the3spaceconstants@gmail.com>
2023-08-09 23:23:52 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+core.register_craft({
+    recipe = {
+        {"default:cobble", "default:cobble", "default:cobble"},
+        {"default:cobble", "default:chest" , "default:cobble"},
+        {"default:cobble", "default:cobble", "default:cobble"},
+    },
+    output = "stone_chests:stone_chest"
+})
